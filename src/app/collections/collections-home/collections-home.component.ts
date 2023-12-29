6ed3acef9f915e53cbc74d39e765301bf5505618
Alexandre Yudi <alexyud@hotmail.com>
2023-12-29 03:40:07 -0300

    Collections and Views modules completed

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './collections-home.component.css'
 })
 export class CollectionsHomeComponent {
+  data = [
+    { name: 'James', age: 24, job: 'Designer', employed: true },
+    { name: 'Jill', age: 26, job: 'Engineer', employed: false },
+    { name: 'Elise', age: 25, job: 'Engineer', employed: true },
+  ];
 
+  headers = [
+    { key: 'employed', label: 'Has a Job?' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ]
 }
